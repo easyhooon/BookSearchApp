281d6f241b6bac6e39dcdf0576d8477f54512dc2
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-01-28 12:31:14 +0900

    fix: loadStateViewHolder 로직 수적
    state 의 대한 괄호 범위가 잘못 지정된 부분 수정

--- presentation/src/main/java/com/kenshi/presentation/adapter/BookSearchLoadStateViewHolder.kt
+++ presentation/src/main/java/com/kenshi/presentation/adapter/BookSearchLoadStateViewHolder.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.kenshi.presentation.R
 import com.kenshi.presentation.databinding.ItemLoadStateBinding
 
+// loagingState 를 관리해주는 adapter 의 뷰홀더
 class BookSearchLoadStateViewHolder(
     private val binding: ItemLoadStateBinding,
     retry: () -> Unit,
@@ -20,10 +21,9 @@ class BookSearchLoadStateViewHolder(
     fun bind(loadState: LoadState) = with(binding) {
         if (loadState is LoadState.Error) {
             tvError.text = itemView.context.getString(R.string.error_occurred)
-
-            progressBar.isVisible = loadState is LoadState.Loading
-            btnRetry.isVisible = loadState is LoadState.Error
-            tvError.isVisible = loadState is LoadState.Error
         }
+        progressBar.isVisible = loadState is LoadState.Loading
+        btnRetry.isVisible = loadState is LoadState.Error
+        tvError.isVisible = loadState is LoadState.Error
     }
 }
