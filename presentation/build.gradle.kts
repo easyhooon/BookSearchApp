4f6d979ff41be32bc211eaf7dee7147f0716a2ce
JI HUN LEE <51016231+easyhooon@users.noreply.github.com>
2023-01-30 16:06:39 +0900

    chore: presentation 모듈 내의 필요없는 의존성 제거

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -50,10 +50,6 @@ dependencies {
     implementation(Dependencies.MATERIAL)
     implementation(Dependencies.CONSTRAINT_LAYOUT)
 
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
     // Lifecycle
     implementation(Dependencies.LIFECYCLE_VIEWMODEL_KTX)
     implementation(Dependencies.LIFECYCLE_RUNTIME_KTX)
